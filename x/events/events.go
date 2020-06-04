8875e19e7a47b23130336373138322b5a50c7c25
Anderson Queiroz <contato@andersonq.eti.br>
2020-06-04 15:29:59 +0200

    x/events: first draft
     - defines an Event type
     - defines an Event Handler interface
     - defines an HandlerBuilder, a helper to add middleware to handlers
     - defines a Consumer interface
     - defines a Kafka consumer implementation
     - TODO: add tests!

---
+++ x/events/events.go
@@ -0,0 +1,45 @@
+package events
+
+import "context"
+
+type Event struct {
+	Headers map[string][]string
+	Payload []byte
+}
+
+type Middleware func(Handler) Handler
+
+type Handler interface {
+	Handle(context.Context, Event) error
+}
+
+type HandlerFunc func(ctx context.Context, e Event)
+
+func (h HandlerFunc) Handle(ctx context.Context, e Event) {
+	h(ctx, e)
+}
+
+type HandlerBuilder struct {
+	middleware  []Middleware
+	rawHandlers []Handler
+}
+
+func (hb HandlerBuilder) UseMiddleware(m ...Middleware) {
+	hb.middleware = append(hb.middleware, m...)
+}
+
+func (hb HandlerBuilder) AddHandler(h Handler) {
+	hb.rawHandlers = append(hb.rawHandlers, h)
+}
+
+func (hb HandlerBuilder) Build() []Handler {
+	var handlers []Handler
+	for _, rh := range hb.rawHandlers {
+		h := rh
+		for _, m := range hb.middleware {
+			h = m(h)
+		}
+		handlers = append(handlers, h)
+	}
+	return handlers
+}
