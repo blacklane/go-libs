214cbc744bb3451f3c7db6997cf2c34125646f31
Mikhail Rakhmanov <rakhmanov.m@gmail.com>
2020-06-09 13:35:45 +0200

    Add more tests

--- x/events/producer_test.go
+++ x/events/producer_test.go
@@ -3,6 +3,7 @@ package events
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
@@ -14,12 +15,16 @@ func createTopic(topic string, t *testing.T) {
 		t.Errorf("failed to create Admin client: %s\n", err)
 		return
 	}
+	maxDuration, err := time.ParseDuration("60s")
+	if err != nil {
+		t.Errorf("failed to parse time.ParseDuration(60s)")
+	}
 	results, err := adminClient.CreateTopics(ctx,
 		[]kafka.TopicSpecification{{
 			Topic:             topic,
 			NumPartitions:     1,
 			ReplicationFactor: 1}},
-		kafka.SetAdminOperationTimeout(1000))
+		kafka.SetAdminOperationTimeout(maxDuration))
 	if err != nil {
 		t.Errorf("failed to create topic: %s", err)
 		return
@@ -36,7 +41,7 @@ func createTopic(topic string, t *testing.T) {
 	adminClient.Close()
 }
 
-func TestProducerProducesEvents(t *testing.T) {
+func TestProducerProducesEventsToCorrectTopic(t *testing.T) {
 	configMap := kafka.ConfigMap{"bootstrap.servers": "localhost"}
 	kafka.NewAdminClient(&configMap)
 	kafkaProducer, err := kafka.NewProducer(&configMap)
@@ -62,5 +67,36 @@ func TestProducerProducesEvents(t *testing.T) {
 		if err := p.Send(e); err != nil {
 			t.Errorf("error sending the event %s", e)
 		}
+	}
+}
+
+func TestProducerProducesEventsToIncorrectTopicWithError(t *testing.T) {
+	configMap := kafka.ConfigMap{"bootstrap.servers": "localhost"}
+	kafka.NewAdminClient(&configMap)
+	kafkaProducer, err := kafka.NewProducer(&configMap)
+	if err != nil {
+		t.Errorf("failed to create producer")
+	}
+	topic := "not_created_topic"
+	messages := map[string]bool{"Anderson": true, "likes": true, "reviewing!": true}
+	handler := ErrorHandlerFunc(func(event *Event, err error) {
+		if event == nil {
+			t.Errorf("failed to parse the event")
+			return
+		}
+		message := string(event.Payload)
+		delete(messages, message)
+	})
+	p := NewKafkaProducer(kafkaProducer, topic, handler)
+	p.Run()
+	for message := range messages {
+		e := Event{Payload: []byte(message)}
+		if err := p.Send(e); err != nil {
+			t.Errorf("error sending the event %s", e)
+		}
+	}
+	p.Shutdown(context.Background())
+	if len(messages) != 0 {
+		t.Errorf("error handler missed %d messages", len(messages))
 	}
 }
