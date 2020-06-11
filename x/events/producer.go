887c7d5aeee08ae42c7d979d26867533aacf403e
Mikhail Rakhmanov <rakhmanov.m@gmail.com>
2020-06-11 18:00:17 +0200

    Fix review comments

--- x/events/producer.go
+++ x/events/producer.go
@@ -2,84 +2,97 @@ package events
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"sync"
-
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"sync"
 )
+
+var ErrProducerNotHandlingMessages = errors.New("producer should be handling messages")
+
+const defaultTimeoutMs = 500
 
 type Producer interface {
 	Send(event Event, topic string) error
-	Run()
+	HandleMessages()
 	Shutdown(ctx context.Context) error
 }
 
 type ErrorHandler interface {
-	HandleError(*Event, error)
+	HandleError(Event, error)
 }
 
-type ErrorHandlerFunc func(*Event, error)
+type ErrorHandlerFunc func(Event, error)
 
-func (eh ErrorHandlerFunc) HandleError(e *Event, err error) {
+func (eh ErrorHandlerFunc) HandleError(e Event, err error) {
 	eh(e, err)
 }
 
-type KafkaProducer struct {
-	runMu        *sync.Mutex
-	producer     *kafka.Producer
-	errorHandler ErrorHandler
-	run          bool
+type kafkaProducer struct {
+	runMutex       *sync.Mutex
+	producer       *kafka.Producer
+	errorHandler   ErrorHandler
+	isRunning      bool
+	flushTimeoutMs int
 }
 
 func NewKafkaProducer(p *kafka.Producer, errorHandler ErrorHandler) Producer {
-	return &KafkaProducer{
-		runMu:        &sync.Mutex{},
-		producer:     p,
-		errorHandler: errorHandler,
-		run:          false,
+	return &kafkaProducer{
+		runMutex:       &sync.Mutex{},
+		producer:       p,
+		errorHandler:   errorHandler,
+		isRunning:      false,
+		flushTimeoutMs: defaultTimeoutMs,
 	}
 }
 
-func (p *KafkaProducer) running() bool {
-	p.runMu.Lock()
-	defer p.runMu.Unlock()
-	return p.run
+func NewKafkaProducerWithTimeout(p *kafka.Producer, errorHandler ErrorHandler, flushTimeoutMs int) Producer {
+	return &kafkaProducer{
+		runMutex:       &sync.Mutex{},
+		producer:       p,
+		errorHandler:   errorHandler,
+		isRunning:      false,
+		flushTimeoutMs: flushTimeoutMs,
+	}
 }
 
-func (p *KafkaProducer) stopRun() {
-	p.runMu.Lock()
-	defer p.runMu.Unlock()
-	p.run = false
+func (p *kafkaProducer) running() bool {
+	p.runMutex.Lock()
+	defer p.runMutex.Unlock()
+	return p.isRunning
 }
 
-func (p *KafkaProducer) startRun() {
-	p.runMu.Lock()
-	defer p.runMu.Unlock()
-	p.run = true
+func (p *kafkaProducer) stopRunning() {
+	p.runMutex.Lock()
+	defer p.runMutex.Unlock()
+	p.isRunning = false
 }
 
-func (p *KafkaProducer) Run() {
+func (p *kafkaProducer) startRunning() {
+	p.runMutex.Lock()
+	defer p.runMutex.Unlock()
+	p.isRunning = true
+}
+
+func (p *kafkaProducer) HandleMessages() {
 	if p.running() {
 		return
 	}
-	p.startRun()
+	p.startRunning()
 	go func() {
 		for e := range p.producer.Events() {
 			switch ev := e.(type) {
 			case *kafka.Message:
-				if p.errorHandler != nil && ev.TopicPartition.Error != nil {
+				if ev.TopicPartition.Error != nil {
 					message := messageToEvent(ev)
-					p.errorHandler.HandleError(message, ev.TopicPartition.Error)
+					p.errorHandler.HandleError(*message, ev.TopicPartition.Error)
 				}
 			}
 		}
 	}()
 }
 
-func (p *KafkaProducer) Send(event Event, topic string) error {
-	if !p.running() {
-		return fmt.Errorf("producer should be running")
-	}
+func (p *kafkaProducer) Send(event Event, topic string) error {
 	return p.producer.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            event.Key,
@@ -87,14 +100,19 @@ func (p *KafkaProducer) Send(event Event, topic string) error {
 	}, nil)
 }
 
-func (p *KafkaProducer) Shutdown(ctx context.Context) error {
-	if !p.running() {
-		return fmt.Errorf("producer should be running")
+func (p *kafkaProducer) Shutdown(ctx context.Context) error {
+	// This method is not responsible for closing the producer
+	// as the producer may be reused by different instance
+	isRunning := p.running()
+	p.stopRunning()
+
+	// Flush will only work if we listen to producer's events
+	if !isRunning {
+		return ErrProducerNotHandlingMessages
 	}
-	defer p.producer.Close()
-	p.stopRun()
+
 	for {
-		notSent := p.producer.Flush(1000)
+		notSent := p.producer.Flush(p.flushTimeoutMs)
 		if notSent == 0 {
 			break
 		}
--- x/events/producer_test.go
+++ x/events/producer_test.go
@@ -2,7 +2,6 @@ package events
 
 import (
 	"context"
-	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -10,65 +9,59 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-func createTopic(topic string) error {
+func createTopic(topic string, t *testing.T) {
+	t.Helper()
+
 	adminClient, err := kafka.NewAdminClient(&kafka.ConfigMap{"bootstrap.servers": "kafka"})
 	if err != nil {
-		return fmt.Errorf("failed to create Admin client: %s", err)
+		t.Fatalf("failed to create Admin client: %s", err)
 	}
 	defer adminClient.Close()
-	maxDuration, err := time.ParseDuration("60s")
-	if err != nil {
-		return fmt.Errorf("failed to parse time.ParseDuration(60s)")
-	}
+
+	maxDuration := 60 * time.Second
 	results, err := adminClient.CreateTopics(context.Background(),
 		[]kafka.TopicSpecification{{
 			Topic:             topic,
 			NumPartitions:     1,
 			ReplicationFactor: 1}},
 		kafka.SetAdminOperationTimeout(maxDuration))
+
 	if err != nil {
-		return fmt.Errorf("failed to create topic: %s", err)
+		t.Fatalf("failed to create topic: %v", err)
 	}
+
 	for _, result := range results {
 		if result.Error.Code() != kafka.ErrNoError &&
 			result.Error.Code() != kafka.ErrTopicAlreadyExists {
-			return fmt.Errorf("topic creation failed for %s: %s",
+			t.Fatalf("topic creation failed for %s: %s",
 				result.Topic, result.Error.String())
 		}
 	}
-
-	return nil
 }
 
 func TestProducerProducesEventsToCorrectTopic(t *testing.T) {
 	kafkaProducer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "kafka"})
 	if err != nil {
-		t.Errorf("failed to create producer")
-		return
+		t.Fatalf("failed to create producer: %v", err)
 	}
+
 	topic := "demo_topic"
-	err = createTopic(topic)
-	if err != nil {
-		t.Errorf("failed to create topic %s", err)
-		return
-	}
+	createTopic(topic, t)
+
 	messages := []string{"Anderson", "likes", "reviewing!"}
-	handler := ErrorHandlerFunc(func(event *Event, err error) {
-		if event == nil {
-			t.Errorf("failed to parse the event")
-			return
-		}
+	handler := ErrorHandlerFunc(func(event Event, err error) {
 		message := string(event.Payload)
-		t.Errorf("failed to deliver the event %s", message)
+		t.Errorf("failed to deliver the event %s: %v", message, err)
 	})
+
 	p := NewKafkaProducer(kafkaProducer, handler)
-	p.Run()
+	p.HandleMessages()
 	defer p.Shutdown(context.Background())
+
 	for _, message := range messages {
 		e := Event{Payload: []byte(message)}
 		if err := p.Send(e, topic); err != nil {
-			t.Errorf("error sending the event %s", e)
-			return
+			t.Errorf("error sending the event %s: %v", e, err)
 		}
 	}
 }
@@ -76,34 +69,36 @@ func TestProducerProducesEventsToCorrectTopic(t *testing.T) {
 func TestProducerProducesEventsToIncorrectTopicWithError(t *testing.T) {
 	kafkaProducer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "kafka"})
 	if err != nil {
-		t.Errorf("failed to create producer")
+		t.Fatalf("failed to create producer: %v", err)
 		return
 	}
+
 	topic := "not_created_topic"
 	messages := map[string]bool{"Anderson": true, "likes": true, "reviewing!": true}
 	mu := sync.Mutex{}
-	handler := ErrorHandlerFunc(func(event *Event, err error) {
-		if event == nil {
-			t.Errorf("failed to parse the event")
-			return
-		}
+
+	handler := ErrorHandlerFunc(func(event Event, err error) {
 		message := string(event.Payload)
+
 		mu.Lock()
+		defer mu.Unlock()
 		delete(messages, message)
-		mu.Unlock()
 	})
+
 	p := NewKafkaProducer(kafkaProducer, handler)
-	p.Run()
+	p.HandleMessages()
 	for message := range messages {
 		e := Event{Payload: []byte(message)}
 		if err := p.Send(e, topic); err != nil {
-			t.Errorf("error sending the event %s", e)
+			t.Errorf("error sending the event %s: %v", e, err)
 		}
 	}
+
 	p.Shutdown(context.Background())
+
 	mu.Lock()
+	defer mu.Unlock()
 	if len(messages) != 0 {
 		t.Errorf("error handler missed %d messages", len(messages))
 	}
-	mu.Unlock()
 }
