0ec2aab99d065c949fe2d8ce8bf9d77edcddcf05
Anderson Queiroz <anderson.queiroz@blacklane.com>
2020-09-10 11:37:18 +0200

    logger: improve events middleware
    * Adds a sub-logger to the context.
    * Extend EventsHandlerStatusLogger to specify which events should be logged. If none is provided, all events are logged.
    * Adds example to EventsHandlerStatusLogger logging only curtain events.
    * Improve docs.
    * Small refactors.

--- logger/middleware/events_test.go
+++ logger/middleware/events_test.go
@@ -65,6 +65,36 @@ func ExampleEventsHandlerStatusLogger_success() {
 	// {"level":"info","application":"ExampleEventsLogger","event":"event_name_here","request_id":"tracking_id-ExampleEventsLogger_Success","tracking_id":"tracking_id-ExampleEventsLogger_Success","duration_ms":1000,"timestamp":"2009-11-10T23:00:01Z","message":"event_name_here succeeded"}
 }
 
+func ExampleEventsHandlerStatusLogger_onlyLogCertainEvents() {
+	// Set current time function so we can control the logged timestamp and duration
+	timeNowCalled := false
+	logger.SetNowFunc(func() time.Time {
+		now := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+		if timeNowCalled {
+			now = now.Add(time.Second)
+		}
+		timeNowCalled = true
+		return now
+	})
+
+	ctx := tracking.SetContextID(context.Background(), "tracking_id-ExampleEventsLogger_Success")
+
+	log := logger.New(os.Stdout, "ExampleEventsLogger")
+
+	hb := events.HandlerBuilder{}
+	hb.UseMiddleware(EventsHandlerStatusLogger("log_event"), EventsAddLogger(log))
+	hb.AddHandler(
+		events.HandlerFunc(func(context.Context, events.Event) error { return nil }))
+
+	h := hb.Build()[0]
+
+	_ = h.Handle(ctx, events.Event{Payload: []byte(`{"event":"log_event"}`)})
+	_ = h.Handle(ctx, events.Event{Payload: []byte(`{"event":"do_not_log_event"}`)})
+
+	// Output:
+	// {"level":"info","application":"ExampleEventsLogger","event":"log_event","request_id":"tracking_id-ExampleEventsLogger_Success","tracking_id":"tracking_id-ExampleEventsLogger_Success","duration_ms":1000,"timestamp":"2009-11-10T23:00:01Z","message":"log_event succeeded"}
+}
+
 func ExampleEventsHandlerStatusLogger_failure() {
 	// Set current time function so we can control the logged timestamp and duration
 	timeNowCalled := false
