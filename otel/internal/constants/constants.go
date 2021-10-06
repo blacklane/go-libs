78e136989e41c5937862f5a1b6713fa74a9f1f3d
Anderson Queiroz <anderson.queiroz@blacklane.com>
2021-10-06 12:03:53 +0200

    remove unused constants

--- otel/internal/constants/constants.go
+++ otel/internal/constants/constants.go
@@ -1,15 +1,7 @@
 package constants
 
-// For a definition of each constant check the logging standards on http://handbook.int.blacklane.io/monitoring/kiev.html
 const (
-	FieldEvent = "event"
-
-	HeaderForwardedFor = "X-Forwarded-For"
-	HeaderRequestID    = "X-Request-Id"
-	HeaderTrackingID   = "X-Tracking-Id"
-
-	TraceID       = "trace_id"
-	TagTrackingID = "tracking_id"
+	TraceID = "trace_id"
 )
 
 const (
