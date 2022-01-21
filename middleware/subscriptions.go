30007df8710c6e7bb1360b6d5c1d20db8400e426
shruthi <shruthi.maganahalli@blacklane.com>
2022-01-21 13:46:17 +0530

    format

--- middleware/subscriptions.go
+++ middleware/subscriptions.go
@@ -9,11 +9,10 @@ import (
 
 func SubscriptionsAddLogger(log logger.Logger) func(camunda.TaskHandler) camunda.TaskHandler {
 	return func(next camunda.TaskHandler) camunda.TaskHandler {
-		h := camunda.TaskHandlerFunc(func(ctx context.Context, completeFunc camunda.TaskCompleteFunc, t camunda.Task) {
+		return camunda.TaskHandlerFunc(func(ctx context.Context, completeFunc camunda.TaskCompleteFunc, t camunda.Task) {
 			log = log.With().Logger() // Creates a sub logger so all requests won't share the same logger instance
 			context := log.WithContext(ctx)
 			next.Handle(context, completeFunc, t)
 		})
-		return h
 	}
 }
