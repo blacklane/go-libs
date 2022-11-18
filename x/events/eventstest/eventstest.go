3e1251f9af0a6ae90d520cc57ba660ffbbcc4554
Juan Carlos Yamacho <juancarlos.yamacho@blacklane.com>
2022-11-18 16:58:05 +0100

    chore: add events consumer example & fix deps

--- x/events/eventstest/eventstest.go
+++ x/events/eventstest/eventstest.go
@@ -1,3 +1,3 @@
 package eventstest
 
-//go:generate go run github.com/golang/mock/mockgen -source=../producer.go -destination=./mock_producer.go -package=eventstest Producer
+//go:generate go run -mod=mod github.com/golang/mock/mockgen -source=../producer.go -destination=./mock_producer.go -package=eventstest Producer
