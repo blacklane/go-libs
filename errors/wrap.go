f3955d2a55dc7e190292683efa5333501d09b318
Juan Carlos Yamacho <juancarlos.yamacho@blacklane.com>
2022-12-14 02:40:06 +0100

    chore(errors): change func to be in only one line

--- errors/wrap.go
+++ errors/wrap.go
@@ -8,6 +8,4 @@ func Is(err, target error) bool { return errors.Is(err, target) }
 
 func As(err error, target interface{}) bool { return errors.As(err, target) }
 
-func Unwrap(err error) error {
-	return errors.Unwrap(err)
-}
+func Unwrap(err error) error { return errors.Unwrap(err) }
