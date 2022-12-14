7c63dfac8215993d2b212da7f16267cfe7137c89
Juan Carlos Yamacho <juancarlos.yamacho@blacklane.com>
2022-12-14 02:37:28 +0100

    chore(errors): rename file

--- errors/encode.go
+++ errors/encode.go
@@ -6,27 +6,23 @@ import (
 	"io"
 )
 
-type errorResponse struct {
-	Errors []*Error `json:"errors"`
-}
-
 func WriteJSON(w io.Writer, err error) {
-	res := &errorResponse{
+	res := struct {
+		Errors []*Error `json:"errors"`
+	}{
 		Errors: innerErrors(err),
 	}
-	if err := json.NewEncoder(w).Encode(res); err != nil {
+	if err := json.NewEncoder(w).Encode(&res); err != nil {
 		panic(err)
 	}
 }
 
 func innerErrors(err error) []*Error {
 	list := make([]*Error, 0)
-	for err != nil {
-		se, ok := err.(*Error)
-		if ok {
+	for ; err != nil; err = errors.Unwrap(err) {
+		if se, ok := err.(*Error); ok {
 			list = append(list, se)
 		}
-		err = errors.Unwrap(err)
 	}
 	return list
 }
